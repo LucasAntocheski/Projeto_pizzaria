b381aed0b7fe277e12a5274d4b4f1c6f6c99c933
LucasAntocheski <108685793+LucasAntocheski@users.noreply.github.com>
2022-07-04 13:39:40 -0300

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $endereco = strtoupper(fgets(STDIN));               //
                                                           //
 ///////////////////////PROCESSOS/////////////////////////|||
 $cardapio = [0,"calabresa" => 35.90,"peperone" => 39.99 ,"toscana" => 40.00 ,"mussarela"=> 25.00 ,
-    "portuguesa" => 45.00, "refrigerante" => 10.00];
+    "portuguesa" => 45.00, "refrigerante" => 10.00]; //associando o Indice (nome da pizza) da Array ao respectivo valor (=>)
 $total = 0;
 $pergunta = "s";
 
@@ -23,16 +23,17 @@ refrigerante = R$ 10,00|". PHP_EOL . PHP_EOL;
 
         echo "Escolha seu pedido: ";
         $pedido = strtolower(trim(fgets(STDIN)));
-        $total = $total + $cardapio[$pedido];
+        $total = $total + $cardapio[$pedido]; // O cálculo será feito : A soma dele mesmo + o valor do índice que o usuário colocar
         echo "Valor do pedido R$" . $total . PHP_EOL;
         echo "Deseja colocar mais algo em seu pedido? {s/n}: ";
         $pergunta = strtolower(trim(fgets(STDIN)));
-        $listapedido[] = $pedido;
+        $listapedido[] = $pedido; // Aqui será armazenada em uma Array todos os pedidos realizados no loop (do_while)
 
     if ($pergunta != "s") {
         echo "Olá ". $nome;
         echo " Itens de seu pedido:". PHP_EOL;
-        print_r($listapedido). PHP_EOL;
+        print_r($listapedido). PHP_EOL; // Aqui será mostrado a array dos pedidos que foram feito de forma mais amigável.
+        //var_dump($listapedido); Método alteranativo para exibir a lista
         echo "seu endereço é :". $endereco. "seu numero de contato é:". $telefone. PHP_EOL;
         echo "O valor total de sua compra é de:". $total;
         break;
