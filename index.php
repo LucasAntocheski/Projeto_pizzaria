82a3cc50b6472e7206c048e2290266669eea0ce0
LucasAntocheski <108685793+LucasAntocheski@users.noreply.github.com>
2022-07-04 13:31:28 -0300

    Create index.php
    Programa em script básico de pedidos de pizzaria em PHP sem armazenamento de SQL.

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+echo "Bem vindo a Pizzaria de Antocheski" . PHP_EOL;
+///////////////////////COLETA DE DADOS////////////////////////|||
+echo "Digite seu Nome: ";                                      //
+$nome = strtoupper(fgets(STDIN));                       //
+                                                              //
+echo "Digite seu Telefone: ";                                //
+$telefone = (fgets(STDIN));                           //
+                                                            //
+echo "Digite seu Endereço: ";                              //
+$endereco = strtoupper(fgets(STDIN));               //
+                                                          //
+///////////////////////PROCESSOS/////////////////////////|||
+$cardapio = [0,"calabresa" => 35.90,"peperone" => 39.99 ,"toscana" => 40.00 ,"mussarela"=> 25.00 ,
+    "portuguesa" => 45.00, "refrigerante" => 10.00];
+$total = 0;
+$pergunta = "s";
+
+do {
+    echo "Cardápio:
+|Calabresa = R$ 35,90 | Peperone = R$ 39,99| Toscana = R$ 40,00| Mussarela = R$ 25,00| Portuguesa = R$ 45,00|
+refrigerante = R$ 10,00|". PHP_EOL . PHP_EOL;
+
+        echo "Escolha seu pedido: ";
+        $pedido = strtolower(trim(fgets(STDIN)));
+        $total = $total + $cardapio[$pedido];
+        echo "Valor do pedido R$" . $total . PHP_EOL;
+        echo "Deseja colocar mais algo em seu pedido? {s/n}: ";
+        $pergunta = strtolower(trim(fgets(STDIN)));
+        $listapedido[] = $pedido;
+
+    if ($pergunta != "s") {
+        echo "Olá ". $nome;
+        echo " Itens de seu pedido:". PHP_EOL;
+        print_r($listapedido). PHP_EOL;
+        echo "seu endereço é :". $endereco. "seu numero de contato é:". $telefone. PHP_EOL;
+        echo "O valor total de sua compra é de:". $total;
+        break;
+    }
+}while ($pergunta != "n");
